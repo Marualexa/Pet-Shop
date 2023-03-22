65ba167468583341fff2edd1991c5defa1c49175
Mariana <contrerasomanam@gmail.com>
2023-03-22 17:17:34 -0500

    implementacion de validacion de input

--- src/components/AddItems.vue
+++ src/components/AddItems.vue
@@ -1,7 +1,7 @@
 <template>
   <LoadingProduct v-if="!loadingProduct && errorData" />
   <ErrorComponent :title="title" :Message="Message" v-if="!errorData && loadingProduct" />
-  <form @submit.prevent="createButton" class="container-items">
+  <form @submit.prevent="createButton" class="container-items" novalidate>
     <div class="form-row">
       <div class="form-group col-md-6">
         <label for="inputEmail4">Name</label>
@@ -17,11 +17,12 @@
         <label for="inputPassword4">Price</label>
         <input
           type="number"
-          class="form-control is-invalid"
+          class="form-control"
           v-model="price"
           placeholder="Price"
+          :class="{ 'is-invalid': isInvalid }"
         />
-        <div class="invalid-feedback">
+        <div v-if="invalid" class="invalid-feedback">
           This container should only have alpha numeric features.
         </div>
       </div>
@@ -36,7 +37,7 @@
       <label for="inputAddress2">Url Pinteres</label>
       <input
         type="text"
-        class="form-control is-invalid"
+        class="form-control"
         v-model="imagen"
         placeholder="Url Pinteres"
       />
@@ -54,11 +55,12 @@
 <script setup>
 import { useAsync } from "../hooks/useAsync";
 import { useRouter } from "vue-router";
-import { ref } from "vue";
+import { computed, ref, watch } from "vue";
 import LoadingProduct from "./LoadModel.vue";
 import ErrorComponent from "./ErrorComponent.vue";
+import RefeImag from "@/assets/imagen.svg"
 
-const { result, makeRequest } = useAsync();
+const { result, makeRequest, errorData } = useAsync();
 const router = useRouter();
 const title = ref("Ooops!");
 const Message = ref("Something went wrong the site did not charge properly 😣😣");
@@ -67,25 +69,51 @@ const imagen = ref(null);
 const price = ref(null);
 const name = ref(null);
 const description = ref(null);
-const imageSrc = ref('@/assets/imagen.svg');
+const imageSrc = ref(RefeImag);
+const invalid = ref(false)
+const isInvalid = ref(false);
+
+const expressions = {
+  name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+  price: /^[0-9]+([.])?([0-9]+)?$/,
+  description: /^[a-zA-ZÀ-ÿ\s]{1,100}$/,
+}
 
 async function createButton() {
-  loadingProduct.value = true;
-  await makeRequest("product", {}, "POST", {
-    imagen: imagen.value,
-    price: price.value,
-    title: name.value,
-    description: description.value,
-    imageSrc: cargImg.value
-  });
-  imagen.value = "";
-  price.value = "";
-  name.value = "";
-  description.value = "";
-  cargImg.value = "";
-  loadingProduct.value = false;
-  router.push({ name: "DetalleProduct", params: { id: result.value.id } });
+if((expressions.price).test(price.value)) {
+  isInvalid.value = false;
+} else {
+  invalid.value = true;
+  isInvalid.value = true;
+  console.log('expresion invalidad')
 }
+ //const isNegative = computed(() => amount.value < 0);
+
+  // loadingProduct.value = true;
+  // await makeRequest("product", {}, "POST", {
+  //   imagen: imagen.value,
+  //   price: price.value,
+  //   title: name.value,
+  //   description: description.value,
+  // });
+  // imagen.value = "";
+  // price.value = "";
+  // name.value = "";
+  // description.value = "";
+  // loadingProduct.value = false;
+  // router.push({ name: "DetalleProduct", params: { id: result.value.id } });
+}
+
+watch(
+  () => imagen.value,
+  (val) => {
+    if(val) {
+      imageSrc.value = val
+    }
+    console.log("Previsualuzacion imagen", imagen);
+
+  }
+);
 </script>
 
 <style scoped>
@@ -120,14 +148,17 @@ async function createButton() {
   display: flex;
   justify-self: end;
   align-items: end;
-  width: 30px;
-  height: 50px;
+  width: 50px;
+  height: 100%;
   border: 1px solid #a75d5d;
+  padding: 23px;
 }
 
 .card-img-top {
   width: 100%;
+  height: 100%;
   border-top-left-radius: calc(0.25rem - 1px);
   border-top-right-radius: calc(0.25rem - 1px);
+  object-fit: cover;
 }
 </style>
